9e7fd39305a9fc91d96958c831438da0e7cefa52
Clahryott <ck.truscott@gmail.com>
2023-02-07 16:17:21 +0800

    all up to date

--- playground-starter/script.js
+++ playground-starter/script.js
@@ -1,5 +1,35 @@
+// function showName() {
+//     let username = document.getElementById("name-text").value;
+//     console.log(`Hi, ${username}`);
+
+// for (let count = 0; count < 10; count++) {
+//     console.log(count);
+// }
+
+// lets pet =  ["dog" "cat", "bird", "fish"];
+// console.log(pets);
+// pets.push("hamster");
+// console.log(pets);
+// console.log(pets[0]);
+// console.log(pets[3]);
+
+// for (let index =0; index < pets.length; index++) {
+//     console.log(pets[index]);
+// }
+
+
 function showName() {
     let username = document.getElementById("name-text").value;
     console.log(`Hi, ${username}`);
+}
 
+let child = "Koko";
+
+if (child === "Wolfgang") {
+    console.log("Wolfgang is the best child in the world!");
+}   else if (child === "Koko") {
+    console.log("Koko is the second best child in thr world")
+
+} else {
+    console.log("The child is not Wolfgang and I guess that's OK!");
 }
--- shopping-list-starter/script.js
+++ shopping-list-starter/script.js
@@ -1,3 +1,24 @@
-function addItem() {}
 
-function updateItems() {}
+//function removeItems() {
+  //  let item = document.getElementById("shopping-list").value;
+  //  shoppingListItems.remove(item); 
+  //  deleteItems();
+// }
+
+// assign an empty list - 
+
+function showName() {
+    let username = document.getElementById("name-text").value;
+    console.log(`Hi, ${username}`);
+}
+
+let child = "Koko";
+
+if (child === "Wolfgang") {
+    console.log("Wolfgang is the best child in the world!");
+}   else if (child === "Koko") {
+    console.log("Koko is the second best child in thr world")
+
+} else {
+    console.log("The child is not Wolfgang and I guess that's OK!");
+}
